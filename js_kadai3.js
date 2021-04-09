9078b639a9fa59e5ab3618f665ca3248f0080bac
yuki-manabi <hy.invest1001@gmail.com>
2021-04-09 22:38:27 +0900

    modify items pointed out 20210408

--- js_kadai3.js
+++ js_kadai3.js
@@ -1,40 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
+let taskList = [];
 
-    const parentNode = document.getElementById('task-table');
-    let taskID = 0;
+document.addEventListener('DOMContentLoaded', () => {
 
     //追加ボタン押下時
     document.getElementById('btn_add').addEventListener('click', () => {
+        
+        let inputContent = document.getElementById('content').value;
 
-        const trElement = document.createElement('tr');
-        let task = document.getElementById('task').value;
-
-        //空の入力時
-        if (task === '') {
-            console.log('input nothing');
+        let addedListNum = addTask(inputContent);
+        if (addedListNum === -1) {
             return;
         }
+        document.getElementById('content').value = '';//追加後に入力を空にする
 
-        const tdIdElement = document.createElement('td');
-        tdIdElement.textContent = taskID++;
-        trElement.appendChild(tdIdElement);
-
-        const tdTaskElement = document.createElement('td');
-        tdTaskElement.textContent = task;
-        trElement.appendChild(tdTaskElement);
-
-        const tdStatusElement = document.createElement('td');
-        const btnProgressElement = document.createElement('button');
-        btnProgressElement.textContent = '作業中';
-        tdStatusElement.appendChild(btnProgressElement);
-        trElement.appendChild(tdStatusElement);
-
-        const btnDeleteElement = document.createElement('button');
-        btnDeleteElement.textContent = '削除';
-        trElement.appendChild(btnDeleteElement);
-
-        parentNode.appendChild(trElement);
-
-        document.getElementById('task').value = '';//追加後に入力を空にする
+        showTask(addedListNum);
     });
 });
+
+function addTask(inputContent) {
+    //空の入力時
+    if (inputContent === '') {
+        console.log('input nothing');
+        return -1;
+    }
+
+    const task = {
+        content: inputContent,
+        status: '作業中'
+    };
+
+    taskList.push(task);
+    return taskList.length - 1;//配列の最後に追加されるため
+}
+
+function showTask(addedListNum) {
+    const parentNode = document.getElementById('task-table');
+
+    const trElement = document.createElement('tr');
+
+    const tdIdElement = document.createElement('td');
+    tdIdElement.textContent = addedListNum;
+    trElement.appendChild(tdIdElement);
+
+    const tdTaskElement = document.createElement('td');
+    tdTaskElement.textContent = taskList[addedListNum].content;
+    trElement.appendChild(tdTaskElement);
+
+    //状態ボタンの作成とHTML作成
+    const tdStatusElement = document.createElement('td');
+    tdStatusElement.appendChild(showStatusButton(addedListNum));
+    trElement.appendChild(tdStatusElement);
+
+    //削除ボタンの作成とHTML作成
+    trElement.appendChild(showDeleteButton(addedListNum));
+
+    parentNode.appendChild(trElement);
+}
+
+function showStatusButton(addedListNum) {
+    const btnProgressElement = document.createElement('button');
+    btnProgressElement.textContent = taskList[addedListNum].status;
+    return btnProgressElement;
+}
+
+function showDeleteButton() {
+    const btnDeleteElement = document.createElement('button');
+    btnDeleteElement.textContent = '削除';
+    return btnDeleteElement;
+}
