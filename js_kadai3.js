7bee1a53df2347a9fd69f906b464e7fa7889ec23
yuki-manabi <hy.invest1001@gmail.com>
2021-04-17 07:43:29 +0900

    js kadai 3-4

--- js_kadai3.js
+++ js_kadai3.js
@@ -2,6 +2,9 @@ const taskList = [];
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    //初期テーブルの作成
+    initialTable();
+
     //追加ボタン押下時
     document.getElementById('btn_add').addEventListener('click', () => {
         
@@ -13,9 +16,73 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         document.getElementById('content').value = '';//追加後に入力を空にする
 
-        showTask(addedTaskId);
+        if (getRadioBtnChecked() !== 'done') { //完了選択時以外に追加タスクを表示する
+            showTask(addedTaskId);
+        }
+    });
+
+    //すべて 押下時
+    document.getElementById('all').addEventListener('click', () => {
+        filterTasks('すべて');
+    });
+
+    //作業中 押下時
+    document.getElementById('in-progress').addEventListener('click', () => {
+        filterTasks('作業中');
+    });
+
+    //完了 押下時
+    document.getElementById('done').addEventListener('click', () => {
+        filterTasks('完了');
     });
 });
+
+function initialTable() {
+    const parentNode = document.getElementById('task-table');
+    const trElement = document.createElement('tr');
+    const thElement1 = document.createElement('th');
+    thElement1.textContent = 'ID';
+    const thElement2 = document.createElement('th');
+    thElement2.textContent = 'コメント';
+    const thElement3 = document.createElement('th');
+    thElement3.textContent = '状態';
+    trElement.appendChild(thElement1);
+    trElement.appendChild(thElement2);
+    trElement.appendChild(thElement3);
+    parentNode.appendChild(trElement);
+}
+
+function getRadioBtnChecked() {
+    const elements = document.getElementsByName('status');
+    let radioChecked = 0;
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            radioChecked = i;
+        }
+    }
+    if (radioChecked === 0) {
+        return 'all';
+    } else if (radioChecked === 1) {
+        return 'in-progress';
+    } else {
+        return 'done';
+    }
+}
+
+
+function filterTasks(inputStatus) {
+    const parentNode = document.getElementById('task-table');
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+    initialTable();
+    for (let i = 0; i < taskList.length; i++) {
+        if (inputStatus === 'すべて' || inputStatus === taskList[i].status) {
+            showTask(i);
+        }
+    }
+
+}
 
 function addTask(inputContent) {
     //空の入力時
@@ -70,13 +137,21 @@ function showButton(btnLabel, taskId, clickFunction) {
 function deleteTask(btnElement, taskId) {
     const parentNode = document.getElementById('task-table');
 
-    for (let i = taskId; i < taskList.length; i++) {
-        parentNode.removeChild(parentNode.lastChild);
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
     }
-    
     taskList.splice(taskId, 1);
-    for (let i = taskId; i < taskList.length; i++) {
-        showTask(i);
+    initialTable();
+
+    const radioChecked = getRadioBtnChecked();
+    for (let i = 0; i < taskList.length; i++) {
+        if (radioChecked === 'all') {
+            showTask(i);
+        } else if (taskList[i].status === '作業中' && radioChecked === 'in-progress') {
+            showTask(i);
+        } else if (taskList[i].status === '完了' && radioChecked === 'done') {
+            showTask(i);
+        }
     }
 }
 
@@ -88,5 +163,8 @@ function changeStatus(btnElement, taskId) {
     } else {
         taskList[taskId].status = '作業中';
         btnElement.textContent = '作業中';
+    }
+    if (getRadioBtnChecked() !== 'all') {
+        btnElement.parentNode.parentNode.remove();
     }
 }
